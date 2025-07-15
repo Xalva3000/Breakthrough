1762185a283e9df9fa5a6869d0a1298176f7471e
Xalva3000 <maasania@gmail.com>
2025-07-15 22:42:05 +0400

    crud added

--- source/api/api_v1/short_urls/views.py
+++ source/api/api_v1/short_urls/views.py
@@ -1,21 +1,23 @@
 from typing import Annotated
 
-from pydantic import AnyHttpUrl
-
-from annotated_types import Len
+# from pydantic import AnyHttpUrl
+# from annotated_types import Len
 
 from .schemas import ShortUrl, ShortUrlCreate
 from fastapi.responses import RedirectResponse
-from .dependencies import prefetch_short_url, SHORT_URLS
+from .dependencies import prefetch_short_url, storage
 from fastapi import Request, HTTPException, status, Depends, APIRouter, Form
 
 
 router = APIRouter(prefix="/short-urls")
 
 
-@router.get("/list/")
-def read_short_urls_list():
-    return SHORT_URLS
+@router.get(
+    "/list/",
+    response_model=list[ShortUrl]
+)
+def read_short_urls_list() -> list[ShortUrl]:
+    return storage.get_all()
 
 
 @router.get("/{slug}")
@@ -24,7 +26,8 @@ def redirect_short_url(slug, url: Annotated[ShortUrl, Depends(prefetch_short_url
     if url:
         return RedirectResponse(url=url.target_url)
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"URL {slug!r} not found"
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"URL {slug!r} not found"
     )
 
 
@@ -36,8 +39,36 @@ def redirect_short_url(slug, url: Annotated[ShortUrl, Depends(prefetch_short_url
 def create_short_url(
     short_url_create: ShortUrlCreate,
 ):
-    print(short_url_create)
-    print(type(short_url_create))
-    return ShortUrl(
+    new_url = ShortUrl(
         **short_url_create.model_dump(),
     )
+
+    storage.create(new_url)
+
+    return new_url
+
+
+@router.delete(
+    "/{slug}/",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "SHORT_URL not found",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "URL 'slug' not found"
+                    }
+                }
+            }
+        }
+    }
+)
+def delete_short_url(
+    url: Annotated[
+        ShortUrl,
+        Depends(prefetch_short_url)
+    ]
+) -> None:
+    # storage.delete_by_slug(slug)
+    storage.delete(short_url=url)
